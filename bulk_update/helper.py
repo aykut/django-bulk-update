3c704df09e559698f0c7722e1b290c2026e04811
Aykut Özat <aykut.ozat@metglobal.com>
2014-04-30 22:16:05 +0300

    code refactor

--- bulk_update/helper.py
+++ bulk_update/helper.py
@@ -2,12 +2,16 @@ from collections import defaultdict
 
 from django.db import connections
 from django.db.models.fields import AutoField
+from django.utils.functional import SimpleLazyObject
 
 
 def bulk_update(objs, update_fields=None, exclude_fields=None,
-                using='default'):
+                using='default', batch_size=None):
+    assert batch_size is None or batch_size > 0
     if not objs:
         return
+
+    batch_size = batch_size or len(objs)
 
     ref_obj = objs[0]
     meta = ref_obj._meta
@@ -25,22 +29,25 @@ def bulk_update(objs, update_fields=None, exclude_fields=None,
     for obj in objs:
         pks.append(obj.pk)
         for field in fields:
-            default = case_clauses.setdefault(
-                field.column, '{column} = (CASE {pkcolumn} {{when}}'.format(
+            _default = SimpleLazyObject(
+                lambda: '{column} = (CASE {pkcolumn} {{when}}'.format(
                     column=field.column, pkcolumn=meta.pk.column))
-            case_clauses[field.column] = default.format(
+
+            _tail = case_clauses.setdefault(field.column, _default)
+            case_clauses[field.column] = _tail.format(
                 when="WHEN %s THEN %s {when}")
             paramaters.extend([obj.pk, getattr(obj, field.attname)])
 
     values = ', '.join(
-        map(lambda x: x.format(when=' END)'), case_clauses.values()))
+        map(lambda v: v.format(when=' END)'), case_clauses.values()))
+    del case_clauses
 
     pkcolumn = meta.pk.column
     dbtable = meta.db_table
     paramaters.extend([tuple(pks)])
 
-    sql = "UPDATE %(dbtable)s SET %(values)s WHERE %(pkcolumn)s" % {
-        'dbtable': dbtable, 'values': values, 'pkcolumn': pkcolumn}
-    sql += " in %s"
+    sql = 'UPDATE {dbtable} SET {values} WHERE {pkcolumn} in %s'.format(
+        dbtable=dbtable, values=values, pkcolumn=pkcolumn)
+    del values, pks
 
     connection.cursor().execute(sql, paramaters)
