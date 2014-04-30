b784445a91192bd04e179f7b76ef54f367b94707
Aykut Özat <aykut.ozat@metglobal.com>
2014-04-30 07:59:58 +0300

    change version to 2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-bulk-update',
-    version='0.1.1',
+    version='0.2',
     packages=find_packages(),
     include_package_data=True,
     description='Bulk update for django.',
