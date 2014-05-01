c0de9f04a1db3706fbd405cf2f9be1ebc89a443b
Aykut Özat <aykut.ozat@metglobal.com>
2014-05-01 11:20:33 +0300

    add batch_size option

--- bulk_update/helper.py
+++ bulk_update/helper.py
@@ -10,7 +10,7 @@ def bulk_update(objs, update_fields=None, exclude_fields=None,
     assert batch_size is None or batch_size > 0
 
     batch_size = batch_size or len(objs)
-
+    connection = connections[using]
     ref_obj = objs[0]
     meta = ref_obj._meta
     exclude_fields = exclude_fields or []
@@ -20,35 +20,43 @@ def bulk_update(objs, update_fields=None, exclude_fields=None,
         meta.fields)
     fields = filter(lambda f: not f.attname in exclude_fields, fields)
 
-    pks = []
-    connection = connections[using]
-    case_clauses = defaultdict(dict)
-    for obj in objs:
-        pks.append(obj.pk)
-        for field in fields:
-            column = field.column
-            _default = SimpleLazyObject(
-                lambda: '{column} = (CASE {pkcolumn} {{when}}'.format(
-                    column=column, pkcolumn=meta.pk.column))
+    def _batched_update(objs, fields, batch_size, connection):
+        pks = []
+        case_clauses = defaultdict(dict)
+        for obj in objs[:batch_size]:
+            pks.append(obj.pk)
+            for field in fields:
+                column = field.column
+                _default = SimpleLazyObject(
+                    lambda: '{column} = (CASE {pkcolumn} {{when}}'.format(
+                        column=column, pkcolumn=meta.pk.column))
 
-            case_clauses.setdefault(column, {'sql': _default, 'params': []})
+                case_clauses.setdefault(
+                    column, {'sql': _default, 'params': []})
 
-            case_clauses[column]['sql'] = case_clauses[column]['sql']\
-                .format(when="WHEN %s THEN %s {when}")
-            case_clauses[column]['params'].extend(
-                [obj.pk, getattr(obj, field.attname)])
+                case_clauses[column]['sql'] = case_clauses[column]['sql']\
+                    .format(when="WHEN %s THEN %s {when}")
+                case_clauses[column]['params'].extend(
+                    [obj.pk, getattr(obj, field.attname)])
 
-    values = ', '.join(
-        map(lambda v: v['sql'].format(when=' END)'), case_clauses.values()))
-    paramaters = [item for v in case_clauses.values() for item in v['params']]
-    del case_clauses
+        if pks:
+            values = ', '.join(
+                map(lambda v: v['sql'].format(when=' END)'),
+                    case_clauses.values()))
+            paramaters = [item for v in case_clauses.values()
+                          for item in v['params']]
+            del case_clauses
 
-    pkcolumn = meta.pk.column
-    dbtable = meta.db_table
-    paramaters.extend([tuple(pks)])
+            pkcolumn = meta.pk.column
+            dbtable = meta.db_table
+            paramaters.extend([tuple(pks)])
 
-    sql = 'UPDATE {dbtable} SET {values} WHERE {pkcolumn} in %s'.format(
-        dbtable=dbtable, values=values, pkcolumn=pkcolumn)
-    del values, pks
+            sql = 'UPDATE {dbtable} SET {values} WHERE {pkcolumn} in %s'\
+                .format(dbtable=dbtable, values=values, pkcolumn=pkcolumn)
+            del values, pks
 
-    connection.cursor().execute(sql, paramaters)
+            connection.cursor().execute(sql, paramaters)
+
+            _batched_update(objs[batch_size:], fields, batch_size, connection)
+
+    _batched_update(objs, fields, batch_size, connection)
--- bulk_update/manager.py
+++ bulk_update/manager.py
@@ -6,34 +6,3 @@ class BulkUpdateManager(models.Manager):
     def bulk_update(self, objs, update_fields=None, exclude_fields=None):
         bulk_update(objs, update_fields=update_fields,
                     exclude_fields=exclude_fields, using=self.db)
-
-
-def _batched_update(objs, fields, connection, batch_size):
-    pks = []
-    paramaters = []
-    case_clauses = defaultdict(str)
-    for obj in objs[:batch_size]:
-        pks.append(obj.pk)
-        for field in fields:
-            _default = SimpleLazyObject(
-                lambda: '{column} = (CASE {pkcolumn} {{when}}'.format(
-                    column=field.column, pkcolumn=meta.pk.column))
-
-            _tail = case_clauses.setdefault(field.column, _default)
-            case_clauses[field.column] = _tail.format(
-                when="WHEN %s THEN %s {when}")
-            paramaters.extend([obj.pk, getattr(obj, field.attname)])
-
-    values = ', '.join(
-        map(lambda v: v.format(when=' END)'), case_clauses.values()))
-    del case_clauses
-
-    pkcolumn = meta.pk.column
-    dbtable = meta.db_table
-    paramaters.extend([tuple(pks)])
-
-    sql = 'UPDATE {dbtable} SET {values} WHERE {pkcolumn} in %s'.format(
-        dbtable=dbtable, values=values, pkcolumn=pkcolumn)
-    del values, pks
-
-    connection.cursor().execute(sql, paramaters)
