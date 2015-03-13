db00d129905eb43c416403dd1b257d43e2964e25
Harmen Wassenaar <h.b.wassenaar@alumnus.rug.nl>
2015-03-13 11:50:25 +0100

    Added extra tests   - update_fields   - exclude_fields   - a number of field-specific tests   - fix for datetime test

--- tests/models.py
+++ tests/models.py
@@ -5,7 +5,11 @@ class Person(models.Model):
     """
         test model
     """
-    name = models.fields.CharField(max_length=140, blank=True, null=True)
-    created = models.fields.DateTimeField(blank=True, null=True)
-    
+    name      = models.fields.CharField(max_length=140, blank=True, null=True)
+    age       = models.PositiveIntegerField()
+    height    = models.DecimalField(max_digits=3, decimal_places=2)
+    email     = models.EmailField()
+    certified = models.NullBooleanField()
+    created   = models.fields.DateTimeField(blank=True, null=True)
+
     objects = BulkUpdateManager()
