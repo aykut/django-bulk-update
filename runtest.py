1c05b6bff47af5042a97a5019875290d1890258e
Harmen Wassenaar <harmen.wassenaar@target-holding.nl>
2015-03-16 09:51:11 +0100

    modified test script for django 1.6

--- runtest.py
+++ runtest.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 import os
 import sys
-
 import django
-from django.conf import settings
-from django.test.utils import get_runner
 
 if __name__ == "__main__":
     os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
-    django.setup()
+
+    from django.conf import settings
+    from django.test.utils import get_runner
+
+    if django.VERSION >= (1, 7):
+       django.setup()
+
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
     failures = test_runner.run_tests(["tests"])
