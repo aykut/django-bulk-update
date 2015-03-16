65b6d028edc763239a5a4775f58ea39ab0773f6a
Aykut Özat <aykut.ozat@metglobal.com>
2015-03-16 23:38:02 +0200

    tests for integer fields

--- tests/models.py
+++ tests/models.py
@@ -7,8 +7,15 @@ class Person(models.Model):
     """
         test model
     """
+    # integers
+    big_age = models.BigIntegerField()
+    comma_separated_age = models.CommaSeparatedIntegerField(max_length=255)
+    age = models.IntegerField()
+    positive_age = models.PositiveIntegerField()
+    positive_small_age = models.PositiveSmallIntegerField()
+    small_age = models.SmallIntegerField()
+
     name = models.fields.CharField(max_length=140, blank=True, null=True)
-    age = models.PositiveIntegerField()
     height = models.DecimalField(max_digits=3, decimal_places=2)
     email = models.EmailField()
     certified = models.NullBooleanField()
--- tests/tests.py
+++ tests/tests.py
@@ -12,32 +12,44 @@ class BulkUpdateTests(TestCase):
         self.now = timezone.now().replace(microsecond=0)  # mysql doesn't do microseconds. # NOQA
         Person.objects.bulk_create(Person(**person) for person in [
             {
-                'name': 'Mike', 'age': 20, 'height': Decimal('1.81'),
+                'big_age': 59999999999999999, 'comma_separated_age': '1,2,3',
+                'age': -99, 'positive_age': 9999, 'positive_small_age': 299,
+                'small_age': -299, 'name': 'Mike', 'height': Decimal('1.81'),
                 'certified': None, 'created': self.now,
                 'email': 'miketakeahike@mailinator.com',
             },
             {
-                'name': 'Pete', 'age': 33, 'height': Decimal('1.93'),
+                'big_age': 245999992349999, 'comma_separated_age': '6,2,9',
+                'age': 25, 'positive_age': 49999, 'positive_small_age': 315,
+                'small_age': 5409, 'name': 'Pete', 'height': Decimal('1.93'),
                 'certified': True, 'created': self.now,
                 'email': 'petekweetookniet@mailinator.com',
             },
             {
-                'name': 'Ash', 'age': 29, 'height': Decimal('1.78'),
+                'big_age': 9929992349999, 'comma_separated_age': '6,2,9,10,5',
+                'age': 29, 'positive_age': 412399, 'positive_small_age': 23315,
+                'small_age': -5409, 'name': 'Ash', 'height': Decimal('1.78'),
                 'certified': True, 'created': self.now,
                 'email': 'rashash@mailinator.com',
             },
             {
-                'name': 'Mary', 'age': 25, 'height': Decimal('1.65'),
+                'big_age': 9992349234, 'comma_separated_age': '12,29,10,5',
+                'age': -29, 'positive_age': 4199, 'positive_small_age': 115,
+                'small_age': 909, 'name': 'Mary', 'height': Decimal('1.65'),
                 'certified': False, 'created': self.now,
                 'email': 'marykrismas@mailinator.com',
             },
             {
-                'name': 'Sandra', 'age': 30, 'height': Decimal('1.59'),
+                'big_age': 999234, 'comma_separated_age': '12,1,30,50',
+                'age': 1, 'positive_age': 99199, 'positive_small_age': 5,
+                'small_age': -909, 'name': 'Sandra', 'height': Decimal('1.59'),
                 'certified': False, 'created': self.now,
                 'email': 'sandrasalamandra@mailinator.com',
             },
             {
-                'name': 'Crystal', 'age': 27, 'height': Decimal('1.71'),
+                'big_age': 9999999999, 'comma_separated_age': '1,100,3,5',
+                'age': 35, 'positive_age': 1111, 'positive_small_age': 500,
+                'small_age': 110, 'name': 'Crystal', 'height': Decimal('1.71'),
                 'certified': None, 'created': self.now,
                 'email': 'crystalpalace@mailinator.com',
             },
@@ -60,6 +72,88 @@ class BulkUpdateTests(TestCase):
         for idx, person in enumerate(people):
             self.assertEqual(person.name, names[idx])
 
+    def test_big_integer_field(self):
+        people = Person.objects.order_by('pk').all()
+        for idx, person in enumerate(people):
+            person.big_age = idx + 27
+        Person.objects.bulk_update(people)
+
+        people = Person.objects.order_by('pk').all()
+        for idx, person in enumerate(people):
+            self.assertEqual(person.big_age, idx + 27)
+            self.assertNotEqual(person.age, person.age + 3)
+
+    def test_comma_separated_integer_field(self):
+        people = Person.objects.order_by('pk').all()
+        for idx, person in enumerate(people):
+            person.comma_separated_age = str(idx) + ',27'
+        Person.objects.bulk_update(people)
+
+        people = Person.objects.order_by('pk').all()
+        for idx, person in enumerate(people):
+            self.assertEqual(person.comma_separated_age, str(idx) + ',27')
+            self.assertNotEqual(person.big_age, person.big_age + 3)
+
+    def test_integer_field(self):
+        """
+            Positive-int values are saved correctly
+        """
+
+        people = Person.objects.order_by('pk').all()
+        for idx, person in enumerate(people):
+            person.age = idx + 27
+        Person.objects.bulk_update(people)
+
+        people = Person.objects.order_by('pk').all()
+        for idx, person in enumerate(people):
+            self.assertEqual(person.age, idx + 27)
+            self.assertNotEqual(person.small_age, person.small_age + 3)
+
+    def test_positive_integer_field(self):
+        """
+            Positive-int values are saved correctly
+        """
+
+        people = Person.objects.order_by('pk').all()
+        for idx, person in enumerate(people):
+            person.positive_age = idx + 27
+        Person.objects.bulk_update(people)
+
+        people = Person.objects.order_by('pk').all()
+        for idx, person in enumerate(people):
+            self.assertEqual(person.positive_age, idx + 27)
+            self.assertNotEqual(person.small_age, person.small_age + 3)
+
+    def test_positive_small_integer_field(self):
+        """
+            Positive-int values are saved correctly
+        """
+
+        people = Person.objects.order_by('pk').all()
+        for idx, person in enumerate(people):
+            person.positive_small_age = idx + 27
+        Person.objects.bulk_update(people)
+
+        people = Person.objects.order_by('pk').all()
+        for idx, person in enumerate(people):
+            self.assertEqual(person.positive_small_age, idx + 27)
+            self.assertNotEqual(person.small_age, person.small_age + 3)
+
+    def test_small_integer_field(self):
+        """
+            Positive-int values are saved correctly
+        """
+
+        people = Person.objects.order_by('pk').all()
+        for idx, person in enumerate(people):
+            person.small_age = idx + 27
+        Person.objects.bulk_update(people)
+
+        people = Person.objects.order_by('pk').all()
+        for idx, person in enumerate(people):
+            self.assertEqual(person.small_age, idx + 27)
+            self.assertNotEqual(person.age, person.age + 3)
+
     def test_datetime(self):
         """
             Datetime values are saved correctly
@@ -74,20 +168,6 @@ class BulkUpdateTests(TestCase):
         for idx, person in enumerate(people):
             self.assertEqual(person.created,
                              self.now - timedelta(days=1 + idx))
-
-    def test_positive_int(self):
-        """
-            Positive-int values are saved correctly
-        """
-
-        people = Person.objects.order_by('pk').all()
-        for idx, person in enumerate(people):
-            person.age = idx + 27
-        Person.objects.bulk_update(people)
-
-        people = Person.objects.order_by('pk').all()
-        for idx, person in enumerate(people):
-            self.assertEqual(person.age, idx + 27)
 
     def test_decimal(self):
         """
