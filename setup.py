031782fc026aefabd12bc26969d491021fd0d48f
Aykut Özat <aykut.ozat@metglobal.com>
2015-05-24 09:13:35 +0300

    bumped version to 1.1.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-bulk-update',
-    version='1.1.2',
+    version='1.1.3',
     packages=find_packages(),
     include_package_data=True,
     description='Bulk update using one query over Django ORM.',
