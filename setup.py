748b7c0d3e2e7f41028ee6257984693070a1272e
Aykut Özat <aykut.ozat@metglobal.com>
2015-11-26 10:28:45 +0200

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
-version = "1.1.6"
+version = "1.1.7"
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
