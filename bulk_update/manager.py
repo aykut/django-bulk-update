2540b970471ff910d64b32d74eea07af8dc25e1c
Vikas Gulati <vikas@squadrun.co>
2016-04-02 08:05:58 +0530

    [#47] Fix that makes the manager work for using() and read/write router scenarios

--- bulk_update/manager.py
+++ bulk_update/manager.py
@@ -1,11 +1,14 @@
 from django.db import models
+from django.db.models.manager import BaseManager
 from .helper import bulk_update
 
 
-class BulkUpdateManager(models.Manager):
-    def bulk_update(self, objs, update_fields=None,
-                    exclude_fields=None, batch_size=None):
-        return bulk_update(
-            objs, update_fields=update_fields,
-            exclude_fields=exclude_fields, using=self.db,
-            batch_size=batch_size)
+class BulkUpdateQuerySet(models.QuerySet):
+    def bulk_update(self, objs, update_fields=None, exclude_fields=None):
+        self._for_write = True
+        bulk_update(objs, update_fields=update_fields,
+                    exclude_fields=exclude_fields, using=self.db)
+
+
+class BulkUpdateManager(BaseManager.from_queryset(BulkUpdateQuerySet)):
+    pass
