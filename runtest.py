9cf8c70e8f7fd946c6ad28bb9d21c1c16067602a
Arnau Gil <arnau@lead-ratings.com>
2017-05-16 18:10:24 +0200

    Remove deprecated code.

--- runtest.py
+++ runtest.py
@@ -9,8 +9,7 @@ if __name__ == "__main__":
     from django.conf import settings
     from django.test.utils import get_runner
 
-    if django.VERSION >= (1, 7):
-       django.setup()
+    django.setup()
 
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
